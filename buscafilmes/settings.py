d25e95eefd92d077744d1198abf44a5e00b54955
Isaac Sampaio <isaac@gmail.com>
2024-10-28 22:21:02 -0300

    Alterando ref css

--- buscafilmes/settings.py
+++ buscafilmes/settings.py
@@ -95,7 +95,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'pesquisa/static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'pesquisa/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
