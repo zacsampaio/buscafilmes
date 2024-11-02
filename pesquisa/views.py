d74ef514141954d58da62957beead60a247158e6
Isaac Sampaio <isaac@gmail.com>
2024-11-02 19:20:02 -0300

    Nova API - Portugues

--- pesquisa/views.py
+++ pesquisa/views.py
@@ -7,12 +7,19 @@ def buscar_filmes(request):
   if request.method == 'POST':
     titulo = request.POST.get('titulo')
     try:
-      response = requests.get(f'http://www.omdbapi.com/?apikey=97ed8a41&t={titulo}&type=movie')
-      filme = response.json()
-      if filme['Response'] == 'False':
+      response = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key=53fcec6ea9b78cbac654e8d369e770d4&query={titulo}&language=pt-BR')
+      dados = response.json()
+      filmes = dados.get('results')
+      if not filmes:
         erro = 'Filme não encontrado!'
-    except:
-      erro = 'Erro: falha na conexão'
+      else:
+        filme = filmes[0]
+    except requests.exceptions.HTTPError as http_err:
+      erro = f'Erro HTTP: {http_err}'
+    except requests.exceptions.RequestException as req_err:
+      erro = f'Erro na requisição: {req_err}'
+    except Exception as e:
+      erro = f'Erro interno: {str(e)}'
   return render (request, 'index.html', {'filme': filme, 'erro': erro})  
     
 def index(request):
